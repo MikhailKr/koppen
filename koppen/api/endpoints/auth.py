8725677051faefc21e1f3c22c6422f36d0fdd799
mikhailk <krasilnikov_mi@rtsoft.ru>
2025-07-01 01:19:44 +0300

    fix import

--- koppen/api/endpoints/auth.py
+++ koppen/api/endpoints/auth.py
@@ -1,5 +1,5 @@
 from fastapi.security import OAuth2PasswordRequestForm
-from koppen.core.auth import authenticate_user, create_access_token
+from core.auth import authenticate_user, create_access_token
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.db import get_async_session
